52fd6058beb27eb7f7cae3e27af7242e5ce1097c
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-02-06 17:21:16 +0300

    Introduction to Kotlin

--- Temel Kotlin/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Temel Kotlin/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    fun isimDegis(view : View){
+        textView.text = "Ataberk Erday"
     }
 }
