bff7b6dd1e11eb0d85fa57504fd12a8b2d099396
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-02-08 04:34:56 +0300

    Kotlin 103 Super Character creator application

--- Kotlin 103 UI/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Kotlin 103 UI/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    fun superKahramanYap(view : View){
+        val isim = isimText.text.toString()
+        val yas = yasText.text.toString().toIntOrNull()
+        val meslek = meslekText.text.toString()
+
+        if(yas == null){
+            sonucText.text= "Dogru yas giriniz "
+        }else{
+            val mySuperKahraman = superKahraman(isim,yas,meslek)
+
+            sonucText.text = "Isim : ${mySuperKahraman.isim} Yas : ${mySuperKahraman.yas} Meslek : ${mySuperKahraman.meslek}"
+        }
+
+
+
+
     }
 }
---
+++ Kotlin 103 UI/app/src/main/java/com/example/myapplication/superKahraman.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication
+
+class superKahraman(var isim :String, var yas:Int, var meslek:String){
+}
