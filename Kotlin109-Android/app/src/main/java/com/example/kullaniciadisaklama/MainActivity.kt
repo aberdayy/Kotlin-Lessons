8f9d950bf8be465967fc47fb2bc4df4f8ccf985c
ABErdayy <83737058+ABErdayy@users.noreply.github.com>
2023-05-19 16:04:51 +0300

    Kotlin 109 | Introduction to SharedPreferences | save-read-delete data |

--- Kotlin109-Android/app/src/main/java/com/example/kullaniciadisaklama/MainActivity.kt
+++ Kotlin109-Android/app/src/main/java/com/example/kullaniciadisaklama/MainActivity.kt
@@ -6,30 +6,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.datastore.preferences.core.Preferences
 import kotlinx.android.synthetic.main.activity_main.*
+
+var alinanKullaniciAdi:String? = null
+val KEY_NAME = "kullanici"
 
 class MainActivity : AppCompatActivity() {
     //Create object
     //lateinit objenin tanimlanmasini ancak degerinin sonradan atanmasini belirlerken ise yarar
-    lateinit var sharedPreferences : SharedPreferences
-    var alinanKullaniciAdi:String? = null
-    val PREFS_FILE = "com.example.kullaniciadisaklama"
-    val KEY_NAME = "kullanici"
+    //lateinit var sharedPreferences : SharedPreferences
+    // sharedPreferences artik kullanilmadigi icin DatStore kullaniyoruz.
+    //var sharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE)
+    //getPreferences => tek bir dosya ile calisir deafult calisir bu sebeple dosya adi verilmesi gerekmez
+    lateinit var sharedPref : SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var sharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE)
-
-        alinanKullaniciAdi = sharedPreferences.getString(KEY_NAME,"empty")
+        sharedPref = applicationContext.getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
+        alinanKullaniciAdi = sharedPref.getString(KEY_NAME,null)
 
         if (alinanKullaniciAdi != null){
             textView.text = "Kaydedilen kullanici adi: ${alinanKullaniciAdi}"
         }
     }
     fun kaydet(view: View){
+
         val kullaniciAdi = editText.text.toString()
-        val editor = sharedPreferences.edit()
+        val editor = sharedPref.edit()
 
         if(kullaniciAdi.isEmpty()){
             Toast.makeText(this,"Lutfen bir deger giriniz!", Toast.LENGTH_LONG).show()
@@ -39,10 +44,22 @@ class MainActivity : AppCompatActivity() {
             textView.text = "Kaydedilen Kullanici Adi : " + kullaniciAdi
 
         }
-
-
     }
+
     fun sil(view: View){
+
+        val editor = sharedPref.edit()
+        alinanKullaniciAdi = sharedPref.getString(KEY_NAME,null)
+
+        if (alinanKullaniciAdi == null){
+            Toast.makeText(this,"Silinecek bir deger bulunamadi!", Toast.LENGTH_LONG).show()
+        }else{
+            textView.text = "Kaydedilen Kullanici Adi : "
+            editor.remove(KEY_NAME)
+            editor.apply()
+        }
+
+
 
     }
 }
